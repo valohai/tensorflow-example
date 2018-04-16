f5812574368bd6be459efbbeab677b999f2ee9e4
Aarni Koskela <akx@iki.fi>
2018-04-16 11:14:41 +0300

    Add Predictor and CLI for it

---
+++ predict_cli.py
@@ -0,0 +1,14 @@
+import argparse
+
+from PIL import Image
+
+from tf_mnist.predict import Predictor
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('files', nargs='*')
+    args = ap.parse_args()
+    predictor = Predictor()
+    for filename in args.files:
+        img = Image.open(filename)
+        print(filename, predictor.predict_digit(img))
